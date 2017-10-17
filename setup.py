51d45268bfb15b36b5297bb44088465e7e041b49
Kamil Bobrowski <kamil.bobrowski@gmail.com>
2017-10-17 23:35:12 +0200

    added setup

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+setup(
+        name='tunepy',
+        packages=['tunepy'],
+        version='0.01',
+        description='Interactive function argument tuning',
+        author='Kamil Bobrowski',
+        author_email='kamil.bobrowski@gmail.com',
+        url='https://github.com/kbobrowski/tunepy',
+        download_url='https://github.com/kbobrowski/tunepy/archive/0.01.tar.gz',
+        keywords=['optimization', 'tuning', 'interactive'],
+        classifiers=[],
+)
+
+
+
