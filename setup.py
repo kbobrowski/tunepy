a4f2c4f4ebab640685ca27bd4a19a48df6850c4d
Kamil Bobrowski <kamil.bobrowski@gmail.com>
2017-10-18 00:15:19 +0200

    update setup

--- setup.py
+++ setup.py
@@ -1,14 +1,37 @@
 from distutils.core import setup
+
+long_decription = \
+"""
+Python decorator which allows to interactively tune arguments of a function.
+Automatically determines output type (matplotlib / numpy image / return value / console output).
+Decorated function can take instances of a "tunable" class (which determines range of the variable) instead of constant values as arguments.
+"""
+
 setup(
         name='tunepy',
-        packages=['tunepy'],
-        version='0.01',
+        version='1.0.dev1',
         description='Interactive function argument tuning',
+        long_description=long_description,
+        url='https://github.com/kbobrowski/tunepy',
         author='Kamil Bobrowski',
         author_email='kamil.bobrowski@gmail.com',
-        url='https://github.com/kbobrowski/tunepy',
-        download_url='https://github.com/kbobrowski/tunepy/archive/0.01.tar.gz',
-        keywords=['optimization', 'tuning', 'interactive'],
+        license='MIT',
+        classifiers=[
+            'Development Status :: 3 - Alpha',
+            'Intended Audience :: Developers',
+            'Intended Audience :: Education',
+            'Intended Audience :: Science/Research',
+            'License :: OSI Approved :: MIT License',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.2',
+            'Programming Language :: Python :: 3.3',
+            'Programming Language :: Python :: 3.4',
+            'Programming Language :: Python :: 3.5',
+            'Programming Language :: Python :: 3.6'
+        ],
+        keywords='optimization visualization tuning interactive'],
+        packages=['tunepy'],
+        python_requires='>=3',
         classifiers=[],
 )
 
